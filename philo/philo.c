b96c93066002f061e7e8181c0012bf14c5790ca6
achanel <achanel@student.21-school.ru>
2021-11-15 16:21:04 +0300

    f

--- philo/philo.c
+++ philo/philo.c
@@ -6,39 +6,48 @@
 /*   By: achanel <achanel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 10:21:33 by achanel           #+#    #+#             */
-/*   Updated: 2021/11/15 16:08:14 by achanel          ###   ########.fr       */
+/*   Updated: 2021/11/15 16:19:45 by achanel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static void	mut_destroy(t_base *base)
+{
+	pthread_mutex_destroy(&base->lock);
+	pthread_mutex_destroy(&base->print);
+	base->i = -1;
+	while (++base->i < base->ph_number)
+		pthread_mutex_destroy(&base->fork[base->i]);
+}
+
 static void	solve(t_base *base)
 {
 	pthread_t	waiter;
 
-	pthread_mutex_init(&base->lock, NULL);
-	pthread_mutex_init(&base->print, NULL);
+	if (pthread_mutex_init(&base->lock, NULL) != 0 )
+		ft_error("MUTEX_INIT ERROR");
+	if (pthread_mutex_init(&base->print, NULL) != 0)
+		ft_error("MUTEX_INIT ERROR");
 	base->i = -1;
 	while (++base->i < base->ph_number)
-		pthread_mutex_init(&base->fork[base->i], NULL);
+		if (pthread_mutex_init(&base->fork[base->i], NULL) != 0)
+			ft_error("MUTEX_INIT ERROR");
 	base->i = 0;
 	while (base->i < base->ph_number)
 	{
 		pthread_mutex_lock(&base->lock);
-		pthread_create(&base->philosopher[base->i],
-			NULL, phil_life, (void *)base);
+		if (pthread_create(&base->philosopher[base->i],
+				NULL, phil_life, (void *)base) != 0)
+			ft_error("THREAD ERROR");
 		pthread_detach(base->philosopher[base->i]);
 		pthread_mutex_lock(&base->lock);
 		base->i++;
 		pthread_mutex_unlock(&base->lock);
 	}
-	pthread_mutex_destroy(&base->lock);
 	pthread_create(&waiter, NULL, waiter_life, (void *)base);
 	pthread_join(waiter, NULL);
-	pthread_mutex_destroy(&base->print);
-	base->i = -1;
-	while (++base->i < base->ph_number)
-		pthread_mutex_destroy(&base->fork[base->i]);
+	mut_destroy(base);
 }
 
 static void	base_init(t_base *base, int ac, char **av)
